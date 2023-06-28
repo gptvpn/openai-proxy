3dc40706136b8d8959be4fbb90db3f48e0ea4134
gptvpn <137971746+gptvpn@users.noreply.github.com>
2023-06-28 11:12:20 +0800

    Update deno.ts

--- deno.ts
+++ deno.ts
@@ -1,4 +1,4 @@
-import { serve } from "https://deno.land/std@0.181.0/http/server.ts";
+import { serve } from "https://deno.land/std/http/server.ts";
 
 const OPENAI_API_HOST = "api.openai.com";
 
@@ -6,9 +6,9 @@ serve(async (request) => {
   const url = new URL(request.url);
 
   if (url.pathname === "/") {
-    return fetch(new URL("./Readme.md", import.meta.url));
+    return await fetch(new URL("./Readme.md", import.meta.url));
   }
 
   url.host = OPENAI_API_HOST;
-  return await fetch(url, request);
+  return await fetch(url.href, request);
 });
